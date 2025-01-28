2a8672059f38dfbebd71e400033c93c3aa409bd1
JustDevelop10 <139110802+Tuna3458@users.noreply.github.com>
2025-01-28 20:40:43 +0300

    Add files via upload

---
+++ yeah.py
@@ -0,0 +1,20 @@
+import os
+x = input("Do you think PC's are cool(Y/N)")
+if x == "Y":
+    print("Yeahh true answer")
+
+if x == "N":
+    print("Okay i dont delete it(for this time)")
+    print("Anddd linux should accept this request,this mean is LİNUX BETTER MAN(ı writed this code on Windows)")
+    print("       _______")
+    print("     .-        -.")
+    print("    /            \ ")
+    print("   |              |")
+    print("   |,  .-.  .-.  ,|")
+    print("   | )(_o/  \o_)( |")
+    print("   |/     /\     \|")
+    print("   (_     ^^     _)")
+    print("    \__|IIIIII|__/")
+    print("     | \IIIIII/ |")
+    print("     \          /")
+    print("      `--------`")
